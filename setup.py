d399c6d3eccd4176f2c7a6c55ed73b00496b3a2a
Enigmage <alizaidi0786.az@gmail.com>
2020-06-24 00:32:58 +0530

    changed url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup( name = 'pluck',
        version = '0.2',
        description = 'Terminal based dictionary/thesaurus',
-       url = 'https://github.com/Enigmage/lookup.git',
+       url = 'https://github.com/Enigmage/pluck.git',
        author = 'Ali Zaidi',
        author_email = 'alizaidi0786.az@gmail.com',
        license = 'GNU General Public License v2.0',
