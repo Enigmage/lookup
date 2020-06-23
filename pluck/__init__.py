266a0b552307175d3d89162805112f9f514b233e
Enigmage <alizaidi0786.az@gmail.com>
2020-06-23 20:09:07 +0530

    added main

--- pluck/__init__.py
+++ pluck/__init__.py
@@ -1,49 +1,43 @@
-import sys
 import requests 
 from bs4 import BeautifulSoup as beSo
 from colorama import Fore,Style
 
-try:
-    if(sys.argv[1] == "-s"):
-
-        loadPage = requests.get(f"https://www.thesaurus.com/browse/{sys.argv[2]}")
-        src = loadPage.content
-        soup = beSo(src, 'html.parser')
-        myText = soup.find_all("a", {"class":"css-r5sw71-ItemAnchor etbu2a31"})
-        print(f"{Fore.CYAN}{Style.BRIGHT}Synonyms of {sys.argv[2]}:-")
-        for num, txt in enumerate(myText, 1):
-            print(f"{Fore.GREEN}{Style.BRIGHT}{num}: {txt.text}")
-    
-    elif(sys.argv[1] == "-a"):
-               
-        loadPage = requests.get(f"https://www.thesaurus.com/browse/{sys.argv[2]}")
-        src = loadPage.content
-        soup = beSo(src, 'html.parser')
-        myText = soup.find_all("a", {"class":"css-hobnle-ItemAnchor etbu2a31"})
-        print(f"{Fore.CYAN}{Style.BRIGHT}Antonyms of {sys.argv[2]}:-")
-
-        for num, txt in enumerate(myText, 1):
-            print(f"{Fore.RED}{Style.BRIGHT}{num}: {txt.text}")
-
-    elif(sys.argv[1] == "-m"): 
-
-        loadPage = requests.get(f"https://www.merriam-webster.com/dictionary/{sys.argv[2]}")
-# print(loadPage.status_code == 200) 
-        src = loadPage.content 
-
-        soup = beSo(src, 'html.parser')
-
-        myText = soup.find_all("span", {"class" : "dtText"})
+def scrape(option, word):
+    try:
+        if( option == "-s"):
+            loadPage = requests.get(f"https://www.thesaurus.com/browse/{word}")
+            src = loadPage.content
+            soup = beSo(src, 'html.parser')
+            myText = soup.find_all("a", {"class":"css-r5sw71-ItemAnchor etbu2a31"})
+            print(f"{Fore.CYAN}{Style.BRIGHT}Synonyms of {word}:")
+            
+            for num, txt in enumerate(myText, 1):
+                print(f"{Fore.GREEN}{Style.BRIGHT}{num}: {txt.text}")
         
-        print(f"{Fore.CYAN}{Style.BRIGHT}Meaning of {sys.argv[2]}:- ")
-        for num, txt in enumerate(myText, 1):
-            print(f"{Fore.YELLOW}{Style.BRIGHT}{num}{txt.text}")
+        elif( option == "-a"):            
+            loadPage = requests.get(f"https://www.thesaurus.com/browse/{word}")
+            src = loadPage.content
+            soup = beSo(src, 'html.parser')
+            myText = soup.find_all("a", {"class":"css-hobnle-ItemAnchor etbu2a31"})
+            print(f"{Fore.CYAN}{Style.BRIGHT}Antonyms of {word}:")
 
-    else:
-        print("Usage pluck [OPTIONS] [WORD] \n Examples: \n $ pluck -m good  (Fetches meaning of good) \n $ pluck -s good  (Fetch synonyms of good) \n $ pluck -a good  (Fetch antonyms of good) ")
+            for num, txt in enumerate(myText, 1):
+                print(f"{Fore.RED}{Style.BRIGHT}{num}: {txt.text}")
 
-except IndexError:
+        elif( option == "-m"): 
+            loadPage = requests.get(f"https://www.merriam-webster.com/dictionary/{word}")
+            src = loadPage.content 
+            soup = beSo(src, 'html.parser')
+            myText = soup.find_all("span", {"class" : "dtText"})      
+            print(f"{Fore.CYAN}{Style.BRIGHT}Meaning of {word}: ")
+            
+            for num, txt in enumerate(myText, 1):
+                print(f"{Fore.YELLOW}{Style.BRIGHT}{num}{txt.text}")
 
-    raise SystemExit("Usage pluck [OPTIONS] [WORD] \n Examples: \n $ pluck -m good  (Fetches meaning of good) \n $ pluck -s good  (Fetch synonyms of good) \n $ pluck -a good  (Fetch antonyms of good) ")
+        else:
+            print("Usage pluck [OPTIONS] [WORD] \n Examples: \n $ pluck -m good  (Fetches meaning of good) \n $ pluck -s good  (Fetch synonyms of good) \n $ pluck -a good  (Fetch antonyms of good) ")
+
+    except IndexError:
+        raise SystemExit("Usage pluck [OPTIONS] [WORD] \n Examples: \n $ pluck -m good  (Fetches meaning of good) \n $ pluck -s good  (Fetch synonyms of good) \n $ pluck -a good  (Fetch antonyms of good) ")
 
 
